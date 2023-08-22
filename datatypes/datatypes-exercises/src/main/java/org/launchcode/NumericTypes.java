9001a452f9e1d8536e4eb222c37385203e0dcd9f
Nikolai <nikolaiptukhin@gmail.com>
2023-08-21 22:20:39 -0400

    Exercise 1

---
+++ datatypes/datatypes-exercises/src/main/java/org/launchcode/NumericTypes.java
@@ -0,0 +1,15 @@
+package org.launchcode;
+import java.util.Scanner;
+public class NumericTypes {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter length of the rectangle");
+        Double length = input.nextDouble();
+
+        System.out.println("Please enter width of the rectangle");
+        Double width = input.nextDouble();
+
+        Double area = length*width;
+        System.out.println("The area of the rectangle is" + area);
+    }
+}
